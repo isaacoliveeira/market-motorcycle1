0f6a759930399bc0d960fc24e72083ffc11da193
isaacoliveeira <isaacalves2406@gmail.com>
2024-09-11 14:42:58 -0300

    att pom.xml

--- src/poster/model/Location.java
+++ src/poster/model/Location.java
@@ -1,5 +1,25 @@
 package poster.model;
 
 public class Location {
+    private String cidade;
+    private String UF;
+
+    public Location(String cidade, String uF) {
+        this.cidade = cidade;
+        UF = uF;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getUF() {
+        return UF;
+    }
+    public void setUF(String uF) {
+        UF = uF;
+    }
+
     
 }
