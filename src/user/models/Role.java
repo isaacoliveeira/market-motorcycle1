dbd02fc0f2755a913977cfdf33d60d45ffd1116e
isaacoliveeira <isaacalves2406@gmail.com>
2024-09-12 10:34:17 -0300

    ATT NO USER

---
+++ src/user/models/Role.java
@@ -0,0 +1,3 @@
+public enum Role { //papel
+    COMPRADOR, VENDEDOR;
+}
--- src/user/models/User.java
+++ src/user/models/User.java
@@ -4,11 +4,45 @@ public class User{
     private ProfileName name;
     private ProfileUsername username;
     private ProfilePassword password;
+    private Role role;
 
-    public User(ProfileName name, ProfileUsername username, ProfilePassword password){
+    public User(ProfileName name, ProfileUsername username, ProfilePassword password, Role role){
         this.name = name;
         this.username = username;
         this.password = password;
+        this.role = role;
+    }
+
+    public ProfileName getName() {
+        return name;
+    }
+
+    public void setName(ProfileName name) {
+        this.name = name;
+    }
+
+    public ProfileUsername getUsername() {
+        return username;
+    }
+
+    public void setUsername(ProfileUsername username) {
+        this.username = username;
+    }
+
+    public ProfilePassword getPassword() {
+        return password;
+    }
+
+    public void setPassword(ProfilePassword password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     @Override
