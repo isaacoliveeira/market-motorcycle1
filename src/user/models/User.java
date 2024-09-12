646ad2594d43f5707c69d323440f88d63d757fbb
viniciusdelucena <viniciusmelosegundo@gmail.com>
2024-09-12 14:45:22 -0300

    Tirei get e set do repository e do service

--- src/user/models/User.java
+++ src/user/models/User.java
@@ -1,43 +1,56 @@
 package user.models;
 
-public class User{
+public class User {
     private ProfileName name;
     private ProfileUsername username;
     private ProfilePassword password;
 
-    public User(ProfileName name, ProfileUsername username, ProfilePassword password){
+    public User(ProfileName name, ProfileUsername username, ProfilePassword password) {
         this.name = name;
         this.username = username;
         this.password = password;
+    }
+
+    // Método para comparar se dois usuários têm o mesmo nome de usuário
+    public boolean hasUsername(ProfileUsername username) {
+        return this.username.equals(username);
+    }
+
+    // Método para atualizar as informações do usuário
+    public void update(ProfileName newName, ProfileUsername newUsername, ProfilePassword newPassword) {
+        if (newName != null) {
+            this.name = newName;
+        }
+        if (newUsername != null) {
+            this.username = newUsername;
+        }
+        if (newPassword != null) {
+            this.password = newPassword;
+        }
+    }
+
+    // Método para verificar se o usuário corresponde ao perfil fornecido
+    public boolean matches(User user) {
+        return this.matches(user.username);
+    }
+
+    // Método para verificar se o usuário corresponde ao perfil fornecido
+    public boolean matches(ProfileUsername username) {
+        return this.username.equals(username);
+    }
+
+    // Método para verificar se o usuário corresponde ao perfil fornecido
+    public boolean matches(ProfileName name) {
+        return this.name.equals(name);
+    }
+
+    // Método para verificar se o usuário corresponde ao perfil fornecido
+    public boolean matches(ProfilePassword password) {
+        return this.password.equals(password);
     }
 
     @Override
     public String toString() {
         return "User [name=" + name + ", username=" + username + "]";
     }
-
-    public ProfileName getName() {
-        return this.name;
-    }
-
-    public void setName(ProfileName name) {
-        this.name = name;
-    }
-
-    public ProfileUsername getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(ProfileUsername username) {
-        this.username = username;
-    }
-
-    public ProfilePassword getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(ProfilePassword password) {
-        this.password = password;
-    }
-
 }
--- src/user/repository/UserRepository.java
+++ src/user/repository/UserRepository.java
@@ -1,4 +1,5 @@
 package user.repository;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,39 +11,33 @@ import user.models.User;
 public class UserRepository {
     List<User> users = new ArrayList<>();
 
-    public void addUser(User user) throws Exception{
-        for (User u : users){
-            if(u.getUsername().equals(user.getUsername())) {
+    public void addUser(User user) throws Exception {
+        for (User u : users) {
+            if (u.matches(user)) {
                 throw new Exception("Usuário já existe");
             }
         }
         users.add(user);
     }
 
-    public void removeUser(User user) throws Exception{
-        if (!users.contains(user)) {
-            throw new Exception("Usuário não encontrado"); 
+    public void removeUser(User user) throws Exception {
+        if (!users.remove(user)) {
+            throw new Exception("Usuário não encontrado");
         }
-        users.remove(user);
     }
 
     public User searchUser(ProfileUsername username) throws Exception {
-        for(User user : users){
-            if (user.getUsername().equals(username)){
+        for (User user : users) {
+            if (user.matches(username)) {
                 return user;
             }
         }
         throw new Exception("Usuário não encontrado");
     }
-    
-    //Usuário só poderá atualizar suas informações caso esteja logado, se estiver logado não há necessidade de fazer verificação de senha pois ja foi feita
+
+    // Usuário só poderá atualizar suas informações caso esteja logado, se estiver logado não há necessidade de fazer verificação de senha pois já foi feita
     public void updateUser(ProfileUsername username, ProfileName newName, ProfileUsername newUsername, ProfilePassword newPassword) throws Exception {
         User user = searchUser(username);
-        if (user != null) {
-            if (newName != null) user.setName(newName);
-            if (newUsername != null) user.setUsername(newUsername);
-            if (newPassword != null) user.setPassword(newPassword);
-        }
-}
-
+        user.update(newName, newUsername, newPassword);
+    }
 }
