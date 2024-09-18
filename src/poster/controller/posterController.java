8d05e988dff648085d6d655ef5d85c8bc701d472
Isaac Alves Leal de Oliveira <isaacalves2406@gmail.com>
2024-09-18 19:06:26 -0300

    posterController feito

--- src/poster/controller/posterController.java
+++ src/poster/controller/posterController.java
@@ -11,39 +11,61 @@ import java.util.List;
 
 public class PosterController {
 
+    private Post loggedPost; 
     private PosterService posterService;
 
     public PosterController(PosterService posterService) {
         this.posterService = posterService;
     }
 
-    public void addPost(Title title, Price price, Location location, Description descrition) {
-        Post post = new Post(title, price, location, descrition);
+    public void addPost(Title title, Price price, Location location, Description description) throws Exception {
+        Post post = new Post(title, price, location, description);
         posterService.addPost(post);
     }
 
-    public void removePost(Title title, Price price, Location location) {
-        Post post = new Post(title, price, location, null);
-        posterService.removePost(post);
+    public Boolean selectPost(Title title) throws Exception {
+        List<Post> posts = posterService.searchByTitle(title);
+        if (!posts.isEmpty()) {
+            loggedPost = posts.get(0);
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public void updatePost(Post oldPost, Post newPost) {
-            posterService.updatePost(oldPost, newPost);
+    public void updatePost(Title newTitle, Price newPrice, Location newLocation, Description newDescription) throws Exception {
+        if (loggedPost != null) {
+            Post newPost = new Post(newTitle, newPrice, newLocation, newDescription);
+            posterService.updatePost(loggedPost, newPost);
+        } else {
+            throw new Exception("Nenhum post selecionado para atualizar.");
+        }
     }
 
-    public void searchByTitle(Title title) {
+    public void removePost() throws Exception {
+        if (loggedPost != null) {
+            posterService.removePost(loggedPost);
+        } else {
+            throw new Exception("Nenhum post selecionado para remover.");
+        }
+    }
+
+    public void searchByTitle(Title title) throws Exception {
         List<Post> posts = posterService.searchByTitle(title);
         if (posts.isEmpty()) {
-            System.out.println("Nenhum post encontrado com o titulo fornecido");
+            System.out.println("Nenhum post encontrado com o título fornecido.");
         } else {
             for (Post post : posts) {
-                System.out.println(post);
+                System.out.println("Post encontrado: " + post);
             }
         }
     }
 
-    public void buyPost(Post post) {
-        posterService.buy(post);
+    public void buyPost() throws Exception {
+        if (loggedPost != null) {
+            posterService.buy(loggedPost);
+        } else {
+            throw new Exception("Nenhum post selecionado para compra.");
+        }
     }
-
 }
